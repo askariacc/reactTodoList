c121bb8ab444a65476ca930278e708e91b5e1259
askariacc <aliaskariacc@gmail.com>
2019-06-13 11:50:59 +0430

    fix

--- src/TodoDashboard.js
+++ src/TodoDashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList';
+
 export default class TodoDashboard extends React.Component{
     constructor(props){
         super(props)
